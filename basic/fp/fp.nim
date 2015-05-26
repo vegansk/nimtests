3fcbb4d085fa529b92a7c1ca39f1d68a44837461
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-05-26 14:40:45 +0600

    Eval for option

--- basic/fp/fp.nim
+++ basic/fp/fp.nim
@@ -8,3 +8,7 @@ echo "None=", none[int]() >>= ((x: int) => none[string]())
 echo "Y=" & $(none[int]().getOr(() => 100))
 
 echo "Z=" & some(200).getOr(() => 100).`$`
+
+# Sadly, in 0.11.2 we can't use method call syntax here
+eval[int,void](some(100500), (v: int) => echo "I'm at some " & $v, () => echo "Nope")
+eval[int,void](none[int](), (v: int) => echo "I'm at some " & $v, () => echo "Nope")
--- basic/fp/option.nim
+++ basic/fp/option.nim
@@ -3,7 +3,7 @@ import future, typetraits
 type Option*[T] = object
   case valid: bool
   of true: value: T
-  of false: discard
+  else: discard
 
 converter toBool*[T](v: Option[T]): bool =
   if v.valid: true else: false
@@ -27,3 +27,6 @@ proc getOr*[T](v: Option[T], d: T): T =
 
 proc getOr*[T](v: Option[T], d: void -> T): T =
   if v: v.value else: d()
+
+proc eval*[T,U](v: Option[T], ifSome: T -> U, ifNone: void -> U): U =
+  if v: ifSome(v.value) else: ifNone()
