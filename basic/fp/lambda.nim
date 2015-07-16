21d8de1cd885e35f3b08c1b83a2ba24584d7e18d
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-07-16 15:09:25 +0600

    Added minimal lambda example

---
+++ basic/fp/lambda.nim
@@ -0,0 +1,13 @@
+import future
+
+type Holder[T] = object
+  value*: T
+
+proc newHolder[T](v: T): Holder[T] = Holder[T](value: v)
+
+proc `$`[T](h: Holder[T]): string = "Holder(" & $h.value & ")"
+
+proc map[T,U](h: Holder[T], f: T -> U): Holder[U] = newHolder[U](f(h.value))
+
+echo(newHolder(100).map((x: int) => $(x * 2) & " items"))
+
