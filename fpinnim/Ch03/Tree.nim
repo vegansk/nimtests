360a8884fbb3a52852762f366d4adf1b5cafba7e
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-09-08 10:49:34 +0600

    Added Tree, Ex. 3.25

---
+++ fpinnim/Ch03/Tree.nim
@@ -0,0 +1,34 @@
+import future
+
+{.experimental.}
+
+type
+  TreeNodeKind = enum
+    tnkLeaf, tnkBranch
+  Tree[T] = ref object
+    case kind: TreeNodeKind
+    of tnkLeaf:
+      value: T
+    of tnkBranch:
+      left, right: Tree[T]
+
+proc Leaf[T](value: T): Tree[T] = Tree[T](kind: tnkLeaf, value: value)
+      
+proc Branch[T](left, right: Tree[T]): Tree[T] = Tree[T](kind: tnkBranch, left: left, right: right)
+
+proc `$`(t: Tree): string =
+  case t.kind
+  of tnkLeaf: "Leaf(" & $t.value & ")"
+  else: "Branch(" & $t.left & ", " & $t.right & ")"
+
+# Ex. 3.25
+proc size(t: Tree): int =
+  case t.kind
+  of tnkLeaf: 1
+  else: 1 + size(t.left) + size(t.right)
+
+when isMainModule:
+  let tree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Branch(Leaf(4), Leaf(5))))
+
+  echo tree
+  echo tree.size()
