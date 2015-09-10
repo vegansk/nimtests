2f7fb11595156908db40c577de568399f4a62b39
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-09-10 17:30:42 +0600

    Added Option type from chapter 4

--- fpinnim/Ch04/Option.nim
+++ fpinnim/Ch04/Option.nim
@@ -7,3 +7,35 @@ import future
 {.experimental.}
 {.warning[TypelessParam]: off.}
 
+type
+  OptionKind = enum
+    okNone, okSome
+  Option[T] = object
+    case kind: OptionKind
+    of okNone:
+      discard
+    else:
+      value: T
+
+proc Some[T](value: T): Option[T] = Option[T](kind: okSome, value: value)
+proc None[T](): Option[T] = Option[T](kind: okNone)
+
+proc `==`[T](x, y: Option[T]): bool =
+  if (x.kind, y.kind) == (okNone, okNone): true
+  elif (x.kind, y.kind) == (okSome, okSome): x.value == y.value
+  else: false
+
+proc `$`[T](o: Option[T]): string =
+  case o.kind
+  of okNone: "None()"
+  else: "Some(" & $o.value & ")"
+
+proc isEmpty(o: Option): bool = o.kind == okNone
+
+when isMainModule:
+  let s = Some(123)
+  let n = None[int]()
+
+  echo s.isEmpty, " ", s
+  echo n.isEmpty, " ", n
+
