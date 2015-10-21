4afe4f7b8de8c6a5fe5fefebaf8da4eaec3d9e6f
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-10-21 14:42:50 +0600

    Some more updates

--- lang/macro_args.nim
+++ lang/macro_args.nim
@@ -1,12 +1,13 @@
 # Discussion is here: http://forum.nim-lang.org/t/1725/1#10766
-import macros, future, sequtils
+import macros, future, sequtils, strutils
 
 macro test(xs: static[seq[string]]): stmt =
   result = newStmtList()
   for x in xs:
     let st = quote do:
-      echo `x`
+      echo "Here is ", `x`
     result.add(st)
+    result.add(parseStmt("echo \"And more $1\"" % [x]))
 
 when isMainModule:
   test(@["1", "2", "3"])
