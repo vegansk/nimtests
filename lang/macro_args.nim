4e9646bc8c7ead00b8a444142d020b8f3b81e366
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-10-21 14:39:22 +0600

    Updated macro parameters test

--- lang/macro_args.nim
+++ lang/macro_args.nim
@@ -1,19 +1,13 @@
 # Discussion is here: http://forum.nim-lang.org/t/1725/1#10766
 import macros, future, sequtils
 
-proc mkEchoes(xs: seq[string]): NimNode {.compileTime.} =
+macro test(xs: static[seq[string]]): stmt =
   result = newStmtList()
   for x in xs:
     let st = quote do:
       echo `x`
     result.add(st)
 
-macro test(xs: static[seq[string]]): stmt =
-  result = mkEchoes(xs)
-
-proc getSeq(): seq[string] {.compileTime.} = 
-  result = toSeq(0..100).mapIt(string, $it)
-
 when isMainModule:
   test(@["1", "2", "3"])
-  test(getSeq())
+  test(toSeq(0..100).mapIt(string, $it))
