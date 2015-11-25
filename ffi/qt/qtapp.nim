a712eec3c78aa9ead13d9db8e4036ca9a51ed446
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-11-25 15:39:27 +0600

    Updated qt test

--- ffi/qt/qtapp.nim
+++ ffi/qt/qtapp.nim
@@ -11,15 +11,17 @@ proc initQByteArray(data: cstring, size: cint = -1): QByteArray {.importcpp: "QB
 proc constData(this: QByteArray): cstring {.importcpp: "constData".}
 
 ####################################################################################################
-# QByteArray
+# QString
 proc initQString(): QString {.importcpp: "QString(@)", constructor.}
 proc initQString(s: cstring = ""): QString {.importcpp: "QString(@)", constructor.}
 proc initQString(ba: QByteArray): QString {.importcpp: "QString(@)", constructor.}
 proc toUtf8(this: QString): QByteArray {.importcpp: "toUtf8".}
-    
+proc fromUtf8(s: cstring): QString {.importcpp: "QString::fromUtf8(@)".}
+proc `$`(s: QString): string = $s.toUtf8.constData
+
 suite "Qt tests":
 
   test "QString":
-    let s = initQString("Hello, world!")
-    echo s.toUtf8.constData
-    check: $s.toUtf8.constData == "Hello, world!"
+    let s = "Hello, world!".initQString
+    echo s
+    check: $s == "Hello, world!"
