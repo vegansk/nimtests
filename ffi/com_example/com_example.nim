b3bb284e4a43a1639789e3ef511e904bc3955509
Anatoly Galiulin <vega@eldis.ru>
2015-12-10 17:51:27 +0600

    Added converter to WideCString

--- ffi/com_example/com_example.nim
+++ ffi/com_example/com_example.nim
@@ -10,12 +10,15 @@ else:
 proc getSystemDriveSerial(buff: WideCString, sLen: cint): cint {.importc: "get_system_drive_serial", cdecl.}
 proc initCom(): cint {.importc: "init_com", cdecl.}
 
+template ws(x: untyped): WideCString =
+  cast[WideCString](x[0].addr)
+
 when isMainModule:
   if initCom() != 0:
     quit "Error initializing com"
   var buff: array[0..100, Utf16Char]
-  let res = getSystemDriveSerial(cast[WideCString](buff[0].addr), buff.len.cint)
+  let res = getSystemDriveSerial(buff.ws, buff.len.cint)
   if res == 0:
-    echo "System drive serial is ", cast[WideCString](buff[0].addr)
+    echo "System drive serial is ", buff.ws
   else:
     echo "Error getting drive serial: ", res
