3e978f1614b08be31f78fd1afc7d42e8c9f90b5a
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2016-01-22 14:57:56 +0600

    Updated issue 3758

--- issues/3758/lambda_if.nim
+++ issues/3758/lambda_if.nim
@@ -5,9 +5,29 @@ proc test(v: int, f: int -> string): string = f(v)
 # This works
 # echo 1.test(v => (if v mod 2 == 0: "even" else: "odd"))
 
-echo 1.test(v => (
-  if v mod 2 == 0:
-    "even"
+# echo 1.test(v => (
+#   if v mod 2 == 0:
+#     "even"
+#   else:
+#     "odd"
+# ))
+
+var (x, y) = (false, true)
+
+# This works
+# case x
+# of true:
+#   let z = if y: "a" else: "b"
+#   echo z
+# else:
+#   echo "no"
+
+case x
+of true:
+  let z = if y:
+    "a"
   else:
-    "odd"
-))
+    "b"
+  echo z
+else:
+  echo "no"
