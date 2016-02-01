e7a21e7dbe3fff60533fddc091bc85d76951ae96
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2016-02-01 16:56:41 +0600

    Migrated to the new helpers in ex3_4

--- stdlib/iup/tutorial/ex3_4.nim
+++ stdlib/iup/tutorial/ex3_4.nim
@@ -12,16 +12,19 @@ txt.attr.set({
 })
 
 let miOpen = iup.item("Open", nil)
-let miSaveAs = iup.item("Save As...", nil)
+let miSaveAs = iup.item("Save as...", nil)
 let miExit = iup.item("Exit", nil)
 
 let miFont = iup.item("Set font...", nil)
+
+let miFind = iup.item("Find...", nil)
+let miGoTo = iup.item("Go to...", nil)
 
 let miAbout = iup.item("About...", nil)
 
 miOpen.onAction proc(h: auto): auto =
   let dlg = iup.fileDlg()
-  dlg.attr.set({
+  dlg.set({
     "dialogType": "open",
     "extFilter": "Text Files|*.txt|All Files|*.*|" 
   })
@@ -30,15 +33,15 @@ miOpen.onAction proc(h: auto): auto =
   defer: dlg.destroy
 
   if dlg.dlgStatus != -1:
-    let fname = dlg.attr.value
+    let fname = dlg["value"]
     let data = fname.readFile
-    txt.attr.value = data
+    txt["value"] = data
 
   IUP_DEFAULT
 
 miSaveAs.onAction proc(h: auto): auto =
   let dlg = iup.fileDlg()
-  dlg.attr.set({
+  dlg.set({
     "dialogType": "save",
     "extFilter": "Text Files|*.txt|All Files|*.*|" 
   })
@@ -47,8 +50,8 @@ miSaveAs.onAction proc(h: auto): auto =
   defer: dlg.destroy
 
   if dlg.getInt("STATUS") != -1:
-    let fname = dlg.attr.value
-    let data = txt.attr.value
+    let fname = dlg["value"]
+    let data = txt["value"]
     fname.writeFile(data)
 
   IUP_DEFAULT
@@ -57,14 +60,13 @@ miExit.onAction(h => IUP_CLOSE)
 
 miFont.onAction proc(h: auto): auto =
   let dlg = iup.fontDlg()
-  dlg.attr.value = txt.attr.font
+  dlg["value"] = txt["font"].asStr
   
   dlg.popup(IUP_CENTER, IUP_CENTER)
   defer: dlg.destroy
  
   if dlg.dlgStatus != -1:
-    let font = dlg.attr.value
-    txt.attr.font = font
+    txt["font"] = dlg["value"].asStr
 
   IUP_DEFAULT
 
@@ -77,24 +79,27 @@ let fileMenu = iup.menu(miOpen,
                         nil)
 let fileSubMenu = iup.submenu("File", fileMenu)
 
+let editMenu = iup.menu(miFind, miGoTo, nil)
+let editSubMenu = iup.submenu("Edit", editMenu)
+
 let fmtMenu = iup.menu(miFont, nil)
 let fmtSubMenu = iup.submenu("Format", fmtMenu)
 
 let helpMenu = iup.menu(miAbout, nil)
 let helpSubMenu = iup.submenu("Help", helpMenu)
 
-let menu = iup.menu(fileSubMenu, fmtSubMenu, helpSubMenu, nil)
+let menu = iup.menu(fileSubMenu, editSubMenu, fmtSubMenu, helpSubMenu, nil)
 
 let vbox = iup.vbox(txt, nil)
 let dlg = iup.dialog(vbox)
-dlg.hattr.menu = menu
-dlg.attr.set({
+dlg["menu"] = menu
+dlg.set({
   "title": "Simple Notepad",
   "size": "QUARTERxQUARTER"
 })
 
 dlg.showXY(IUP_CENTER, IUP_CENTER)
-dlg.hattr.userSize = nil
+dlg["userSize"] = nil.cstring
 
 iup.mainLoop()
 
